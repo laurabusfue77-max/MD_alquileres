91eb658e1dc71d94b78610df237fcb2e549fc2b5
laurabusfue77-max <laurabusfue.77@gmail.com>
2025-11-24 20:34:42 -0500

    Add files via upload

---
+++ Scripts.js
@@ -0,0 +1,6 @@
+(function addFooter() {
+    const footer = document.createElement("footer");
+    footer.className = "footer-custom";
+    footer.innerHTML = `<p>&copy; 2025 MD Alquileres - Contáctanos al <strong>3153500149</strong></p>`;
+    document.body.appendChild(footer);
+})();
